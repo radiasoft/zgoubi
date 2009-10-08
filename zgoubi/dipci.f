7d5602ac0c0eee38568b26ac8d4af56a4a346514
jsberg-bnl <jsberg-bnl@9fce57e2-1c3b-0410-bf5a-d845d45b8007>
2009-10-08 02:15:01 +0000

    Hollerith cleanup of last commit.

--- zgoubi/dipci.f
+++ zgoubi/dipci.f
@@ -174,14 +174,14 @@ C Entrance EFB
      >        '' first order kick  ***'')')
         WRITE(NRES,103) DXE(KMAG),THETA(KMAG),R1(KMAG),U1(KMAG),
      >          U2(KMAG),R2(KMAG)
- 103    FORMAT(10X,9HDX_face =,F7.2,3H cm,
-     >   5X,15HWedge  angle  =,F7.2,5H deg.,/,
-     1   11X,10HRadius 1 =,1P,G10.2,3H cm,/ ,
-     2   11X,21HStraight  segment 1 =,G10.2,3H cm,/ ,
-     3   11X,21HStraight  segment 2 =,G10.2,3H cm,/ ,
-     4   11X,10HRadius 2 =,G10.2,3H cm)
+ 103    FORMAT(10X,'DX_face =',F7.2,' cm',
+     >   5X,'Wedge  angle  =',F7.2,' deg.',/,
+     1   11X,'Radius 1 =',1P,G10.2,' cm',/ ,
+     2   11X,'Straight  segment 1 =',G10.2,' cm',/ ,
+     3   11X,'Straight  segment 2 =',G10.2,' cm',/ ,
+     4   11X,'Radius 2 =',G10.2,' cm')
         IF(R1(KMAG)*R2(KMAG) .EQ. 0.D0) WRITE(NRES,123)
- 123    FORMAT(10(1H*),' ATTENTION |    R1 OU R2 = 0 |',10(1H*))
+ 123    FORMAT(10('*'),' ATTENTION |    R1 OU R2 = 0 |',10('*'))
       ENDIF
 
 C Exit EFB
@@ -509,7 +509,7 @@ C           REGION DE COURBURE R2
           ELSE
 C           ERREUR  DE  DONNEES  FACE  ENTREE
             IF(NRES.GT.0) WRITE(NRES,104)
- 104        FORMAT(/,5X,10(1H*),' ERREUR PARAMETRES FACE ENTREE',/)
+ 104        FORMAT(/,5X,10('*'),' ERREUR PARAMETRES FACE ENTREE',/)
             IF(NRES.GT.0) WRITE(NRES,134) X,AX,CX,XA,YA,XC,YC
             GOTO  99
           ENDIF
@@ -561,7 +561,7 @@ C           ... REGION DE COURBURE R2
             D=( D * RRS/R + SHIFTS(KMAG) )
           ELSE
             IF(NRES.GT.0) WRITE(NRES,114)
-114         FORMAT(/,5X,10(1H*),' ERREUR PARAMETRES FACE SORTIE',/)
+114         FORMAT(/,5X,10('*'),' ERREUR PARAMETRES FACE SORTIE',/)
             IF(NRES.GT.0) WRITE(NRES,134) X,AXS,CXS,XAS,YAS,XCS,YCS
             GOTO  99
           ENDIF
@@ -615,7 +615,7 @@ C             ... REGION DE COURBURE R2
             ELSE
               IF(NRES.GT.0) THEN
                 WRITE(NRES,124)
-124             FORMAT(/,5X,9(1H*),'ERREUR PARAMETRES FACE LATERALE',/)
+124             FORMAT(/,5X,9('*'),'ERREUR PARAMETRES FACE LATERALE',/)
                 WRITE(NRES,134) X,AX3,CX3,XA3,YA3,XCC,YCC
 134             FORMAT(/,5X,7G12.4)
               ENDIF
