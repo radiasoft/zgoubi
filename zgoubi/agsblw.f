502a218db2529439031e754f3f6249053aa2bfed
fmeot <fmeot@9fce57e2-1c3b-0410-bf5a-d845d45b8007>
2013-11-27 22:11:13 +0000

    Test...

--- zgoubi/agsblw.f
+++ zgoubi/agsblw.f
@@ -6,7 +6,7 @@ C  This program is free software; you can redistribute it and/or modify
 C  it under the terms of the GNU General Public License as published by
 C  the Free Software Foundation; either version 2 of the License, or
 C  (at your option) any later version.
-C
+C 
 C  This program is distributed in the hope that it will be useful,
 C  but WITHOUT ANY WARRANTY; without even the implied warranty of
 C  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
