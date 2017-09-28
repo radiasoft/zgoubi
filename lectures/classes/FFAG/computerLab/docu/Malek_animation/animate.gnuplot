e2a89a3e39874b5ab34283c06ea914fa3a605035
fmeot <fmeot@9fce57e2-1c3b-0410-bf5a-d845d45b8007>
2017-09-28 18:28:32 +0000

    Moved to branches/zgoubi-lecture.

--- lectures/classes/FFAG/computerLab/docu/Malek_animation/animate.gnuplot
+++
@@ -1,5 +0,0 @@
-plot 'animation.fai' u ($10):($38==i?($11):1/0) w p title sprintf("Turn=%i",i)
-print 'i=',i
-pause 1
-i=i+10
-if (i < n) reread
--- lectures/classes/cyclotron/computerLab/ex_1.2.1/1.a/method2_CONSTY/consty2FieldMap.f
+++
@@ -1,5 +0,0 @@
-C Will read X, Y, BZ from zgoubi.plt (filled up using CONSTY), will organize 
-C these data and store as a 2D field map (Z==0) in consty2FieldMap.out.
-C This field map is usable with TOSCA[MOD=25].
-C Could be extended to provide 3D mad, for use with TOSCA[MOD=24].
-
