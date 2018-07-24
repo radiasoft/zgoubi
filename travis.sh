092bdb7780ba987aff2666708c0cc19df63159fa
Izaak Beekman <zbeekman@gmail.com>
2018-07-24 11:26:07 -0400

    Use docker image for CI testing
    Former-commit-id: 3cfd5691f7d42f4ada16908e1130469a3eb3fef2

---
+++ travis.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Perform CI testing in the desired docker image passed as the first argument
+# default to radiasoft/beamsim-part1 if no first argument is passed
+
+set -o pipefail
+set -o nounset
+set -o errexit
+
+docker run -i --rm -u vagrant -v "$PWD":/home/vagrant/src/radiasoft/zgoubi "${1:-radiasoft/beamsim-part1}" bash  <<-'EOF'
+    #!/usr/bin/env bash
+    source ~/.bashrc
+    set -euo pipefail
+    cd ~/src/radiasoft/zgoubi
+    mkdir build > /dev/null 2>&1 || echo \
+          "Warning: Using an old/dirty build directory! Please run 'rm -r build' and try again if script fails." >&2
+    cd build
+    cmake -Wdev -DCMAKE_INSTALL_PREFIX=$(pyenv prefix) ..
+    make -j $(nproc)
+    ctest --output-on-failure
+    make install
+EOF
