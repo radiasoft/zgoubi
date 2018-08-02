19950fee6004e57c28cce2c02652afab7bd60f64
Izaak Beekman <zbeekman@gmail.com>
2018-08-02 15:22:28 -0400

    Fetch/verify test input file & rm unused srcs
     - Fetch WarmSnake.map from the web and verify it's SHA256 checksum
     - Remove LAPACK.f because it's huge and currently unused
    
    
    Former-commit-id: 270cd30e050f635b40c52dfba5581498f9cff5ce

---
+++ cmake/maybe_fetch_file.cmake
@@ -0,0 +1,30 @@
+# Simple function to download assets and verify their checksum
+function(maybe_fetch_file FILE URL SHA256 HAVE_FILE)
+  if( (NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/${FILE}) AND (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${FILE}) )
+    message(STATUS "Copying ${FILE} from the source tree to the build tree.")
+    file(COPY  ${CMAKE_CURRENT_SOURCE_DIR}/${FILE} ${CMAKE_CURRENT_BINARY_DIR}/${FILE})
+  elseif(NOT EXISTS ${CMAKE_BINARY_DIR}/${FILE} )
+    message( STATUS "Fetching ${FILE} from ${URL}")
+    file(DOWNLOAD "${URL}" "${CMAKE_CURRENT_BINARY_DIR}/${FILE}"
+      SHOW_PROGRESS
+      TLS_VERIFY ON
+      EXPECTED_HASH SHA256=${SHA256}
+      )
+  endif()
+  if( EXISTS ${CMAKE_BINARY_DIR}/${FILE} )
+    message( STATUS "Verifying SHA256 for ${FILE}")
+    file(SHA256 ${CMAKE_BINARY_DIR}/${FILE} local_sha256)
+    if( NOT ${local_sha256} STREQUAL ${SHA256} )
+      message( FATAL_ERROR
+	"Checksum for ${FILE} does not match expected value!
+         ${CMAKE_CURRENT_BINARY_DIR}:${FILE} SHA256:
+              ${local_sha256}
+         expected SHA256:
+              ${SHA256}")
+      set(${HAVE_FILE} FALSE PARENT_SCOPE)
+    else()
+      message( STATUS "Checksum matches: ${FILE} SHA256=${local_sha256}")
+      set(${HAVE_FILE} TRUE PARENT_SCOPE)
+    endif()
+  endif()
+endfunction()
