7c56f02e151d19bb6eb2db25ee55d84397452679
Dan T. Abell <ironphoton69@gmail.com>
2018-12-15 06:14:02 +0000

    Set fileencoding to utf-8; strip trailing blanks.

--- zgoubi/accen.f
+++ zgoubi/accen.f
@@ -1,6 +1,6 @@
 C  ZGOUBI, a program for computing the trajectories of charged particles
 C  in electric and magnetic fields
-C  Copyright (C) 1988-2007  François Méot
+C  Copyright (C) 1988-2007  FranÃ§ois MÃ©ot
 C
 C  This program is free software; you can redistribute it and/or modify
 C  it under the terms of the GNU General Public License as published by
@@ -17,8 +17,8 @@ C  along with this program; if not, write to the Free Software
 C  Foundation, Inc., 51 Franklin Street, Fifth Floor,
 C  Boston, MA  02110-1301  USA
 C
-C  François Méot <fmeot@bnl.gov>
-C  Brookhaven National Laboratory      
+C  FranÃ§ois MÃ©ot <fmeot@bnl.gov>
+C  Brookhaven National Laboratory
 C  C-AD, Bldg 911
 C  Upton, NY, 11973
 C  -------
@@ -26,7 +26,7 @@ C  -------
      >                    RATIN)
       IMPLICIT DOUBLE PRECISION (A-H,O-Z)
 C--------------------------------------------------------------------------
-C     Computes the rms ellipse with emittance AY matched to a particle set, 
+C     Computes the rms ellipse with emittance AY matched to a particle set,
 C     and gives particle rate within that ellipse
 C--------------------------------------------------------------------------
       INCLUDE "C.CDF.H"     ! COMMON/CDF/ IES,LF,LST,NDAT,NRES,NPLT,NFAI,NMAP,NSPN,NLOG
@@ -43,7 +43,7 @@ C     $     IREP(MXT),AMQLU,PABSLU
       SAVE EPSPI
 
 Compute rms ellipse
-      CALL LPSFIT(JJ, 
+      CALL LPSFIT(JJ,
      >               EMIT,ALP,BET,XM,XPM)
 Compute number of particles alive and number inside ellipse
       CALL CNTINL(JJ,EPSPI,ALP,BET,XM,XPM,
@@ -55,11 +55,11 @@ C        J2=J1+1
 C        IF(J1.EQ.6) THEN
 C          J1=1
 C          J2=6
-C        ENDIF        
+C        ENDIF
 C        NINW=0
 C        NOFF = 0
 C        DO 1 I=1,IMAX
-C            IF(IEX(I) .LT. -1) THEN 
+C            IF(IEX(I) .LT. -1) THEN
 C              NOFF = NOFF+1
 C              GOTO 1
 C            ENDIF
@@ -68,9 +68,9 @@ C            T2 = F(J2,I)*UNIT(J2-1)
 C            YT = Y2*T2
 C            Y2 = Y2*Y2
 C            T2 = T2*T2
-C            IF( GAM*Y2+2.D0*ALP*YT+BET*T2 .LE. EPSPI) NINW=NINW+1 
+C            IF( GAM*Y2+2.D0*ALP*YT+BET*T2 .LE. EPSPI) NINW=NINW+1
 C 1      CONTINUE
-C----- RATIN is of course at maximum 1. 
+C----- RATIN is of course at maximum 1.
 C      RATIN = DBLE(NINW)/DBLE(IMAX-NOFF)
 C      RATIN = DBLE(NINL)/DBLE(NLIV)
       RATIN = DBLE(NINL)/DBLE(IMAX)
