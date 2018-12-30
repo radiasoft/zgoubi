01a67cf79c6a0692a55fd9596ae06e8aad1a9258
Dan T. Abell <ironphoton69@gmail.com>
2018-12-30 06:34:09 +0000

    More work on duplicate /CONST/.

--- zgoubi/objeta.f
+++ zgoubi/objeta.f
@@ -34,7 +34,7 @@ C                    AM1     ,AM2,     AM3,   AM4       ,    AM5  ,  AM6
 C                  .93828  1.87563   2.80892 .5488,        .10566  .10566
 C     **********************************************
       INCLUDE "C.CDF.H"     ! COMMON/CDF/ IES,LF,LST,NDAT,NRES,NPLT,NFAI,NMAP,NSPN,NLOG
-      INCLUDE "C.CONST.H"     ! COMMON/CONST/ CL9,CL ,PI,RAD,DEG,QE ,AMPROT, CM2M
+      INCLUDE "C.CONST_2.H"     ! COMMON/CONST/ CL9,CL,PI,RAD,DEG,QEL,AMPROT,CM2M
       INCLUDE 'MXLD.H'
       INCLUDE "C.DON.H"     ! COMMON/DON/ A(MXL,MXD),IQ(MXL),IP(MXL),NB,NOEL
       INCLUDE "MAXCOO.H"
