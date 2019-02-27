75713b9e9c7dbb0740a6a597b179f5aec45f095e
Dan T. Abell <ironphoton69@gmail.com>
2019-02-27 21:25:36 +0000

    Trim trailing blanks.

--- zgoubi/integr.f
+++ zgoubi/integr.f
@@ -61,41 +61,41 @@ C      INCLUDE "C.FAISC.H"     ! COMMON/FAISC/ F(MXJ,MXT),AMQ(5,MXT),DP0(MXT),IM
 C      COMMON/SPACECHA/ TSPCH,TLAMBDA,Rbeam,Xave,Emitt,Tave,Bunch_len,
       COMMON/SPACECHA/ TLAMBDA,Rbeam(2),Xave(2),Emitt(2),Tave,Bunch_len,
      >                Emittz, BTAG, SCkx, SCky, TSPCH
-cc      INCLUDE 'MXLD.H'              ! pour faire les tests: to be deleted please 
+cc      INCLUDE 'MXLD.H'              ! pour faire les tests: to be deleted please
 cc      COMMON /LABEL/ LABEL(MXL,2)   ! pour faire les tests: to be deleted please
 C       INCLUDE "C.REBELO.H"   ! COMMON/REBELO/ NRBLT,IPASS,KWRT,NNDES,STDVM
 cccccccccccccccccccccccccccccccccccccccccccccc
 
       LOGICAL WEDGE, WEDGS
       SAVE WEDGE, WEDGS
- 
+
       PARAMETER (DLIM = 1.D-6)
       INCLUDE 'MXSTEP.H'
       PARAMETER (PI24= 2.467401101D0)
- 
+
       LOGICAL BACK, CHREG
- 
+
       SAVE NSTEP
       DIMENSION  AREG(2),BREG(2),CREG(2), AREGI(2),BREGI(2),CREGI(2)
       SAVE AREG,BREG,CREG,KREG
- 
+
       SAVE WDGE, WDGS, FINTE, FINTS, GAPE, GAPS
- 
+
       LOGICAL FITMEM
       SAVE FITMEM
- 
+
       LOGICAL CONSTY, CSTYI
-      SAVE CONSTY 
+      SAVE CONSTY
 
       PARAMETER (KSIZ=10)
       CHARACTER(KSIZ) KEY
 
       DIMENSION F(MXJ,MXT)
       SAVE X0
-      
+
       DATA WEDGE, WEDGS / .FALSE.,  .FALSE./
       DATA WDGE, WDGS, FINTE, FINTS, GAPE, GAPS / 6*0.D0 /
- 
+
       DATA FITMEM / .FALSE. /
       DATA CONSTY / .FALSE. /
 
@@ -103,7 +103,7 @@ cccccccccccccccccccccccccccccccccccccccccccccc
       ISORT=1
       CT=COS(T)
       ST=SIN(T)
- 
+
       IF(KPAS .NE. 0)  THEN
 C------- 3-region step &/or variable step
         IF(TPAS(1) .EQ. 0.D0) THEN
@@ -115,34 +115,34 @@ C--------- Entrance is sharp edge
         PAS=TPAS(KREG)
         DXI = PAS
       ENDIF
- 
+
       NSTEP = 0
       X2 = X
       Y2 = Y
       Z2 = Z
       IF(FITMEM) CALL FITMM2(IT)
       IF(CONSTY) X0 = X
- 
+
 C----- DEBUT DE BOUCLE SUR DXI
 C      Start loop on DXI
     1 CONTINUE
- 
+
 C---------------------  Some tests to possibly stop integration
       NSTEP = NSTEP+1
       IF(NSTEP .GT. MXSTEP) CALL KSTOP(2,IT,KEX,*97)
- 
+
       BACK = T*T .GT. PI24
       IF(.NOT.BACKW) THEN
 C------- PARTICLES BENT BACKWARD THOUGH NOT ALLOWED ARE STOPPED
         IF(BACK) CALL KSTOP(3,IT,KEX,*97)
       ENDIF
 C-------------------------------------------------------------------
- 
+
       IF(KALC .EQ. 2 ) THEN
 C-------  Compute  B(X,Y,0)  from  field  maps
- 
+
         CALL CHAMK(X2,Y2,Z2,*99)
- 
+
         IF(KERK .NE. 0) THEN
 C          IF(ABS(BZ).GT.0.020)  KEX =-1
           IF(ISORT .EQ. 2)  GOTO 7
@@ -155,14 +155,14 @@ C          IF(ABS(BZ).GT.0.020)  KEX =-1
         IF(ISORT .EQ. 1)  GOTO 7
         IF(NRES.GT.0) WRITE(NRES,100) Y,X,T,IT,' came back in.'
         ISORT=1
- 
+
       ELSE
 C-------  Compute  B(X,Y,Z), E(X,Y,Z)  from  mathematical  2D or 3D  field  models
- 
+
         CALL CHAMC(X2,Y2,Z2)
- 
+
       ENDIF
- 
+
 cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 C------- Correction for space charge calculation --Begin--
 cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
@@ -177,11 +177,11 @@ ccccccccccccc   get the path legnth from the first central orbit   ccccccccccccc
                     T1=T
                     Y1=Y
                     Z1=Z
-                   endif 
-           elseif (IT .NE. 1) then                   
+                   endif
+           elseif (IT .NE. 1) then
                    tlength0=0.0
-           endif        
-ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc                
+           endif
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 
 
            if ((IT .NE. 1) .and. (IT .NE. ITinit)) then
@@ -193,25 +193,25 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
              Tsp=SCkx*(Yinit-Xave(1)*100.0)*tlength
              Psp=SCky*(Zinit-Xave(2)*100.0)*tlength
 
-             T=T+Tsp   
+             T=T+Tsp
              P=P+Psp
 
-           endif   
+           endif
 
 cccccccccccccccccccccccccccccccccccccccccccccccccccccc
 cccccccc    write the data to a test4 file   ccccccccc
-cccccccccccccccccccccccccccccccccccccccccccccccccccccc 
+cccccccccccccccccccccccccccccccccccccccccccccccccccccc
 c           if ((IT .EQ. 1) .and. (tlength .LE. 2*PAS)) then
-c             call ZGIPAS( 
+c             call ZGIPAS(
 c     >                   IPASS,NRBLT)
 c              write(88,fmt=
-c     >        '(1p,9(e14.6,1x),i10,1x,e14.6,a)') 
+c     >        '(1p,9(e14.6,1x),i10,1x,e14.6,a)')
 c     >        Rbeam(1),Rbeam(2),Xave(1),SAR,Emitt(1),Emitt(2),Tave,
 c     >        Bunch_len, Emittz,IPASS,BTAG,' integr spach'
-c           endif  
+c           endif
 cccccccccccccccccccccccccccccccccccccccccccccccccccccc
-                  
-          ENDIF   
+
+          ENDIF
 
 
 cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
@@ -220,18 +220,18 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 
 
  7    CONTINUE
- 
+
       IF(NSTEP .EQ. 1) THEN
 C-------- Entrance wedge correction in BEND, in MULTIPOL(if non zero B1),
         IF(WEDGE) CALL WEDGKI(1,T,Z,P,WDGE,FINTE,GAPE)
       ENDIF
 
       CALL DEVTRA
- 
+
       IF(KPAS.NE.0 ) THEN
 C------- Test case diffrnt step values @ entrance|body|exit.
 C         Exist entrance and/or exit fringe field regions and XPAS is coded.
- 
+
 C-------- CHREG is .true. if particle is going to next region
         IF(CHREG(KART,X,Y,AREG,BREG,CREG,DXI,TPAS,
      >                                            KREG,AL,BL,D)) THEN
@@ -249,21 +249,21 @@ C-------- CHREG is .true. if particle is going to next region
             ST=SIN(T)
             PAS = TPAS(KREG)
             DXI = PAS
- 
+
             GOTO 1
         ENDIF
       ENDIF
- 
+
 C FM 08/99      IF(IDRT .GE. 2) THEN
       IF(IDRT .GE. 1) THEN
 C------- DROITE(S) DE COUPURE EN SORTIE
 C        "Droite" has to be intersected by trajectory within X<XLIM (X cannot
 C        be > XLIM)
- 
+
         IF    (KART .EQ. 1) THEN
 C--------- COORDONNEES CARTESIENNES
           D = ABS(CA(KDR)*X + SA(KDR)*Y + CM(KDR))
- 
+
           IF(D .LT. DXI) THEN
             AL = CA(KDR)
             BL = SA(KDR)
@@ -272,10 +272,10 @@ C--------- COORDONNEES CARTESIENNES
             CT = COS(T)
             COSTA = AL*CT + BL*ST
             PAF = D/COSTA
- 
+
             IF(.NOT.MIRROR) THEN
 C------------- Step onto the exit 'DROITE'
- 
+
               CALL DEPLA(PAF)
               COSTA=AL*CT+BL*ST
               CALL ITER(AL,BL,CL,PAF,COSTA,KEX,IT,*97)
@@ -286,7 +286,7 @@ C------------- Step onto the exit 'DROITE'
               ELSEIF(LST .EQ. 3) THEN
                 CALL IMPPLA(  30,Y,T,Z,P,X,SAR,TAR,PAF,AMT,QT,KEX,IT)
               ENDIF
- 
+
 C FM 08/99              IF    (KDR .EQ. IDRT) THEN
               IF    (KDR .GE. IDRT) THEN
 C--------------- i.e., IDRT=1 or 2
@@ -317,45 +317,45 @@ C---------------- There are (IDRT-1)>2 "droites en sorties" (e.g., spectrometer 
               ENDIF
             ENDIF
 C------------- if .not.mirror etc.
- 
+
           ENDIF
- 
+
         ELSEIF( KART .EQ. 2) THEN
 C--------- Cylindrical coordinates
 C Array CA contains angle of integration boundary
- 
+
           CALL ENDJOB(' Sbr integr : '
      >    //'IDRT still to be implemented for polar frame',-99)
- 
+
           IF(ABS(CA(2)-X) .LT. DXI) THEN
- 
+
           ENDIF ! XLIM-X
- 
+
         ENDIF  ! KART
       ENDIF ! IDRT .GE. 1
- 
- 
+
+
 C-----------------------    TEST  EFB's in Multipoles etc.
 C                              IFB set to .ne.0 due to mixed sharp edge+FF
       IF(IFB .NE. 0 ) THEN
 C------- Entrance or exit EFB's
- 
+
         IF( KART .EQ. 1) THEN
 C--------- COORDONNEES CARTESIENNES
- 
+
           KFB = 0
- 
+
           IF( AFB(1)*X + BFB(1)*Y + CFB(1) .LE. 0.D0 ) THEN
- 
+
             IF( IFB .EQ. -1 .OR. IFB .EQ. 2 ) KFB=1
 C----------- EFB entree
- 
+
           ELSEIF(AFB(2)*X + BFB(2)*Y + CFB(2) .LE. 0.D0 ) THEN
- 
+
             IF( IFB .EQ. 1 .OR. IFB .EQ. 2 ) KFB=2
 C----------- EFB sortie
           ENDIF
- 
+
           IF( KFB .NE. 0 ) THEN
             D = ABS(AFB(KFB)*X + BFB(KFB)*Y + CFB(KFB))
             IF(D .LT. DXI) THEN
@@ -377,13 +377,13 @@ C----------- EFB sortie
             ENDIF
           ENDIF
 C------------ kfb .ne. 0
- 
+
         ENDIF
 C---------- kart .eq. 1
- 
+
       ENDIF
 C-------- ifb .ne. 0
- 
+
       DX=XLIM-X
 
       IF(DX/DXI .LT. 1.D0) THEN
@@ -393,18 +393,18 @@ C-------- ifb .ne. 0
           IF(.NOT.BACK) CALL KSTOP(8,IT,KEX,*97)
         ENDIF
       ENDIF
- 
+
       PAF = PAS
       CALL DEPLA(PAF)
- 
+
       CALL COFIN(KART,NPLT,LST,PAF,KEX,IT,AMT,QT,EVNT,
      >                                            Y,T,Z,P,X,SAR,TAR,*97)
- 
+
       IF(FITMEM) CALL FITMM(IT,Y,T,Z,P,SAR,DP,TAR,PAS)
- 
+
       CT=COS(T)
       ST=SIN(T)
- 
+
 C A trick to force coordinates to constant, for tests - Keyword 'OPTIONS'
       IF(CONSTY) THEN
         CALL MAJTR1(
@@ -412,22 +412,22 @@ C A trick to force coordinates to constant, for tests - Keyword 'OPTIONS'
         Y = F(2,IT)
         Z = F(4,IT)
         T = 0.D0
-        P = 0.D0  
+        P = 0.D0
         X = X0 + NSTEP*DXI
            write(*,*) ' integr dxi, x, nstep : ',dxi, x, nstep
       ENDIF
 C-----------------------------------------------------------------------
- 
+
       X2 = X
       Y2 = Y
       Z2 = Z
- 
+
       GOTO 1
 C---------  FIN DE BOUCLE SUR DXI
 C           End loop on DXI
- 
+
  2    CONTINUE
- 
+
       IF(KART .EQ. 1) THEN
         AL=1.D0
         BL=0.D0
@@ -439,19 +439,19 @@ C           End loop on DXI
         CL=BL*Y
         PAF=-CL/CT
       ENDIF
- 
+
       CALL DEPLA(PAF)
       COSTA=AL*CT+BL*ST
       CALL ITER(AL,BL,CL,PAF,COSTA,KEX,IT,*97)
       CALL COFIN(KART,NPLT,LST,PAF,KEX,IT,AMT,QT,EVNT,
      >                                            Y,T,Z,P,X,SAR,TAR,*97)
- 
+
       XFINAL = XLIM
- 
+
  6    CONTINUE
- 
+
       DX=XFINAL-X
- 
+
       IF(ABS(DX) .GT. DLIM) THEN
         CT=COS(T)
         ST=SIN(T)
@@ -480,9 +480,9 @@ c          ELSE
 
         ENDIF
       ENDIF
- 
+
       IF(FITMEM) CALL FITMM(IT,Y,T,Z,P,SAR,DP,TAR,PAS)
- 
+
  99   CONTINUE
 c      IF(KALC .EQ. 2 ) THEN
 c        CALL CHAMK(X,Y,Z,*97)
@@ -495,12 +495,12 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 c        CALL CHAMC(X,Y,Z)
 CCCCCCCCCCCCCC
 c      ENDIF
- 
+
 C-------- Wedge correction in BEND, in MULTIPOL with non zero B1, etc.
       IF(WEDGS) CALL WEDGKI(2,T,Z,P,WDGS,FINTS,GAPS)
- 
+
  97   CONTINUE
- 
+
 C----- Print last step
       IF(LST .GE. 1) THEN
         IF(LST .EQ. 2) THEN
@@ -509,38 +509,38 @@ C----- Print last step
           CALL IMPDEV('INTEGR')
         ENDIF
       ENDIF
- 
+
       RETURN
- 
+
       ENTRY INTEG1(WDGI,FINTEI,GAPEI)
       WEDGE = .TRUE.
       WDGE=WDGI
       FINTE = FINTEI
       GAPE = GAPEI
       RETURN
- 
+
       ENTRY INTEG2(WDGI,FINTSI,GAPSI)
       WEDGS = .TRUE.
       WDGS = WDGI
       FINTS = FINTSI
       GAPS = GAPSI
       RETURN
- 
+
       ENTRY INTEG3
       WEDGE = .FALSE.
       WEDGS = .FALSE.
       RETURN
- 
+
       ENTRY INTEG4(
      >             NSTP)
       NSTP = NSTEP
       RETURN
- 
+
       ENTRY INTEG5(
      >             PASO)
       PASO=PAS
       RETURN
- 
+
       ENTRY INTEG6(AREGI,BREGI,CREGI)
       AREG(1)=AREGI(1)
       BREG(1)=BREGI(1)
@@ -549,7 +549,7 @@ C----- Print last step
       BREG(2)=BREGI(2)
       CREG(2)=CREGI(2)
       RETURN
- 
+
       ENTRY INTEG8(KFITMM)
       FITMEM = KFITMM.EQ.1
       RETURN
@@ -557,5 +557,5 @@ C----- Print last step
       ENTRY INTEGA(CSTYI)
       CONSTY = CSTYI
       RETURN
- 
+
       END
