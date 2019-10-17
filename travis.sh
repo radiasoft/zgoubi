dfcaf5abde20959693e9071f0e937d6256f0f14c
Dan T. Abell <ironphoton69@gmail.com>
2019-10-17 16:07:59 +0000

    Make chromPSR test verbose, to diagnose the failure on Travis.

--- travis.sh
+++ travis.sh
@@ -22,6 +22,7 @@ docker run -i --rm -u vagrant -v "$PWD":/home/vagrant/src/radiasoft/zgoubi "${1:
     cd "${BUILD_DIR:-cmake-build}"
     cmake -Wdev -DCMAKE_INSTALL_PREFIX=$(pyenv prefix) ..
     make -j $(nproc)
-    ctest --output-on-failure
+    #ctest --output-on-failure
+    ctest VERBOSE=ON --output-on-failure
     make install
 EOF
